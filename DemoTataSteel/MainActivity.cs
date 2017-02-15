0bcdecb12b17f03dd5bf325f31d586d97587eaa9
admin <admin@NESARA>
2017-02-15 17:42:12 +0530

    Add project files.

---
+++ DemoTataSteel/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
+namespace DemoTataSteel
+{
+    [Activity(Label = "DemoTataSteel", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            MobileCenter.Start("d51922ec-2b3a-4676-933d-bd4ffafd0c51",
+                    typeof(Analytics), typeof(Crashes));
+        }
+    }
+}
+
